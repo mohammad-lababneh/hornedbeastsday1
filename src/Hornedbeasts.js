b51a8f5f15ec0f8f249382af8e9e5cf1aeb4ab32
mohammad-lababneh <mohammadlababneh96@gmail.com>
2021-06-22 15:40:23 +0300

    add

--- src/Hornedbeasts.js
+++ src/Hornedbeasts.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react'
 
- class Hornedbeasts extends Component {
+class Hornedbeasts extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            galary: 0
+        }
+    }
+    increaseClick = () => {
+        this.setState(
+            {
+                galary: this.state.galary + 1
+            })
+    }
     render() {
+        // console.log(this.state.galary)
+        console.log(this.props)
         return (
             <div>
-              <h1>{this.props.title}</h1>
+                <h1>{this.props.title}</h1>
                 <p>{this.props.description}</p>
                 <h2>{this.props.keyword}</h2>
                 <h3>{this.props.horns}</h3>
-                <img src={this.props.imgUrl} style={{width: 150} }
-/>
-
+                <img src={this.props.imgUrl} style={{ width:250 }}  />
+                <h3>{this.state.galary}</h3>
+ <button onClick={this.increaseClick} style={{ borderRadius: 8 }}>increaseClick</button>
             </div>
         )
     }
--- src/Main.js
+++ src/Main.js
@@ -1,47 +1,47 @@
 import React, { Component } from 'react'
+import { NavItem } from 'reactstrap'
 import Hornedbeasts from './Hornedbeasts'
 import hornsData from './hornsData.json'
 // import React from 'react';
 // import { Button } from 'reactstrap';
 class Main extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            galary: 0
-        }
-    }
 
-    react = () => {
-        this.setState(
-            {
-                galary: this.state.galary + 1
-            })
-    }
+
+
     // const printImages=()
     render() {
-        console.log(typeof this.state.galary);
-        console.log(this.state)
+
 
         return (
             <div>
 
                 {
+
                     hornsData.map((item) => {
                         return <div>
-                            <p>{item.title}</p>
+
+                            <Hornedbeasts 
+                           
+                           imgUrl = {item.image_url}
+                            title={item.title} 
+                             description={item.description}
+                             keyword = {item.keyword}
+                             horns = {item.horns} />
+                            {/* <Hornedbeasts  /> */}
+                            {/* <p>{item.title}</p>
                             <p>{item.description}</p>
                             <p>{item.keyword}</p>
                             <p>{item.horns}</p>
-                            <img src={item.image_url} style={
+                            <img onClick={this.react} src={item.image_url} style={
                                 { width: 300 }} />
-                            <h1>{this.state.galary}</h1>
-                            <button onClick={this.react} style={{ borderRadius: 8 }}>React</button>
+                                {/* <span>{this.state.onClick}</span> */}
+                            {/* <h1>{this.state.galary}</h1> */}
+                            {/* <button onClick={this.react} style={{ borderRadius: 8 }}>React</button> */}
                         </div>
                     })
                 }
-                <hornsData />
-                <Hornedbeasts/>
+
                 {/* <Button/> */}
 
             </div >
